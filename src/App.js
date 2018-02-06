c6cc7e0c0a05dde69123231b993d70da684670c6
Alex Mac <meet.alexmac@gmail.com>
2018-02-05 20:52:16 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import List from './List/list';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text" />
+        <List />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/List/list.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ListItem from './ListItem/list-item';
+class List extends Component {
+  render() {
+    return (
+      <ul>
+        <ListItem />
+      </ul>
+    )
+  }
+}
+
+export default List;
